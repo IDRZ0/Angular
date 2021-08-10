1a205e3661b6ea4cfb6e3872854875baaa47a9a1
IgnacioDelRio <nachitotigredelriozenteno@gmail.com>
2021-08-10 17:41:17 -0400

    10/08/21 banana box examples

--- src/app/test2/test2.component.ts
+++ src/app/test2/test2.component.ts
@@ -12,9 +12,25 @@ export class Test2Component {
 
   @Output() sendData = new EventEmitter();
 
+  myvar1 = 4;
+  myvar2 = 5;
+
   constructor() { }
 
   onClickSend() {
     this.sendData.emit('general data ' + this.name + ' ' + this.lastName);
+  }
+
+  test0(event: any) {
+    console.log("click", event);
+    console.log("suma: ", this.myvar1 + this.myvar2);
+  }
+
+  test1(event: any) {
+    console.log("blur", event);
+  }
+
+  test2(event: any) {
+    console.log("double", event);
   }
 }
