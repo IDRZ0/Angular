b25f38cb9b9c8fab2d6605cfeff3534199257e4b
IgnacioDelRio <nachitotigredelriozenteno@gmail.com>
2021-08-10 18:54:24 -0400

    10/08/21 lifecycle expamle test3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Proyecto';
   myStatus = "busy";
+  varCh = "change";
+  varBool = true;
 
   constructor() {
     const testMap = [1, 2, 3, 4, 5, 6].map(item => item * 2);
--- src/app/test3/test3.component.ts
+++ src/app/test3/test3.component.ts
@@ -1,16 +1,53 @@
-import { Component } from '@angular/core';
+import { AfterViewInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, Component, DoCheck, Input, OnChanges, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'test3',
   templateUrl: './test3.component.html',
   styleUrls: ['./test3.component.scss']
 })
-export class Test3Component {
+export class Test3Component implements OnInit, OnChanges, DoCheck, OnDestroy, AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit {
 
   name = "Ignacio";
   lastName = "del Rio";
 
-  constructor() { }
+  @Input() myvar: string = "";
+
+  constructor() {
+    console.log("Contructor");
+  }
+
+  ngOnInit() {
+    console.log("Init");
+  }
+
+  ngOnChanges(changes: any) {
+    console.log("Changes: ", changes);
+  }
+
+  ngDoCheck() {
+    console.log("Check");
+  }
+
+  ngAfterContentInit() {
+    console.log("Content Init")
+  }
+
+  ngAfterContentChecked() {
+    console.log("Content Check")
+  }
+
+  ngAfterViewInit() {
+    console.log("View Init")
+  }
+
+  ngAfterViewChecked() {
+    console.log("View Check")
+  }
+
+  ngOnDestroy() {
+    console.log("Destroy");
+  }
 
   test0(event: any) {
     console.log("Nombre: ", this.name + ' ' + this.lastName);
