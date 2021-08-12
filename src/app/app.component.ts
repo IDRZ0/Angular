4cff80da028758aa8368baf62f1fcca5d2bb2597
IgnacioDelRio <nachitotigredelriozenteno@gmail.com>
2021-08-12 18:45:01 -0400

    12/08/21 tiktok observer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { of } from 'rxjs'
+import { BehaviorSubject, of, Subscription } from 'rxjs'
 import { map, filter } from 'rxjs/operators'
 
 @Component({
@@ -8,12 +8,16 @@ import { map, filter } from 'rxjs/operators'
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Proyecto';
-  myStatus = "busy";
-  varCh = "change";
-  varBool = true;
-  color: string = "";
+  // title = 'Proyecto';
+  // myStatus = "busy";
+  // varCh = "change";
+  // varBool = true;
+  // color: string = "";
+
+  video: number = 1;
   tiktok = of([1, 2, 3, 4, 5]);
+  ticktok1 = new BehaviorSubject(this.video);
+  personASub: Subscription = new Subscription();
 
   constructor() {
     this.tiktok.subscribe((v) => {
@@ -32,81 +36,101 @@ export class AppComponent {
       console.log("Video: ", v);
     });
 
-    const testMap = [1, 2, 3, 4, 5, 6].map(item => item * 2);
-    console.log(testMap);
+    this.ticktok1.pipe(
+      filter(s => s % 2 === 0)
+    ).subscribe((v) => {
+      console.log("Person B Video: ", v);
+    });
 
-    const testForeach = [1, 2, 3, 4, 5, 6].forEach(item => item * 2);
-    console.log(testForeach);
+    this.personASub = this.ticktok1.subscribe((v) => {
+      console.log("Person A Video: ", v);
+    });
 
-    const testFind = [1, 2, 3, 4, 5, 6].find(item => item === 4);
-    console.log(testFind);
+    // const testMap = [1, 2, 3, 4, 5, 6].map(item => item * 2);
+    // console.log(testMap);
 
-    const testFilter = [1, 2, 3, 4, 5, 6].filter(item => item % 2 === 0);
-    console.log(testFilter);
+    // const testForeach = [1, 2, 3, 4, 5, 6].forEach(item => item * 2);
+    // console.log(testForeach);
 
-    const testFindIndex = [90, 2, 300, 4, 5, 6].findIndex(item => item === 300);
-    console.log(testFindIndex);
+    // const testFind = [1, 2, 3, 4, 5, 6].find(item => item === 4);
+    // console.log(testFind);
 
-    const testIndexOf = 'ricardo'.indexOf('o')
-    console.log(testIndexOf);
+    // const testFilter = [1, 2, 3, 4, 5, 6].filter(item => item % 2 === 0);
+    // console.log(testFilter);
 
-    const testJoin = [1, 2, 3, 4, 5, 6].join(',');
-    console.log(testJoin);
+    // const testFindIndex = [90, 2, 300, 4, 5, 6].findIndex(item => item === 300);
+    // console.log(testFindIndex);
 
-    const testSplit = '1,2,3,4,5,6'.split(',');
-    console.log(testSplit);
+    // const testIndexOf = 'ricardo'.indexOf('o')
+    // console.log(testIndexOf);
 
-    const testSpliceA = [10, 20, 30, 40, 50, 60];
-    const testSpliceB = [1, 2, 3, 4, 5, 6].splice(1, 2);
-    testSpliceA.splice(0, 1)
+    // const testJoin = [1, 2, 3, 4, 5, 6].join(',');
+    // console.log(testJoin);
 
-    console.log(testSpliceB, testSpliceA);
+    // const testSplit = '1,2,3,4,5,6'.split(',');
+    // console.log(testSplit);
 
-    const testReduce = [1, 2, 3, 4, 5, 6].reduce((acc, value) => acc + value, 100);
+    // const testSpliceA = [10, 20, 30, 40, 50, 60];
+    // const testSpliceB = [1, 2, 3, 4, 5, 6].splice(1, 2);
+    // testSpliceA.splice(0, 1)
 
-    const asadasd = { 1: 'd', 2: 'd', 3: 'd', 4: 'd', 5: 'd' };
+    // console.log(testSpliceB, testSpliceA);
 
-    const testEntries = { value: 'ignacio', key: 'f' };
-    console.log('aaaa', Object.entries(testEntries));
-    console.log('bbbbb', Object.keys(testEntries));
-    console.log('ccccc', Object.values(testEntries));
+    // const testReduce = [1, 2, 3, 4, 5, 6].reduce((acc, value) => acc + value, 100);
 
-    const array1 = [1, 2, 3, 4, 5];
-    const array2 = [9, 10, 11, ...array1]
+    // const asadasd = { 1: 'd', 2: 'd', 3: 'd', 4: 'd', 5: 'd' };
 
-    const per1 = { name: 'a', age: 12 };
-    const per2 = { data: 'R', ...per1 };
+    // const testEntries = { value: 'ignacio', key: 'f' };
+    // console.log('aaaa', Object.entries(testEntries));
+    // console.log('bbbbb', Object.keys(testEntries));
+    // console.log('ccccc', Object.values(testEntries));
 
-    console.log('Spread', array2, per2)
+    // const array1 = [1, 2, 3, 4, 5];
+    // const array2 = [9, 10, 11, ...array1]
 
-    const per3 = {
-      name: 'a',
-      age: 12,
-      phone: 12323234234,
-      extra: 123,
-      response: 200,
-      response2: 200,
-      response4: 200
-    };
+    // const per1 = { name: 'a', age: 12 };
+    // const per2 = { data: 'R', ...per1 };
 
-    const { phone: ci } = per3;
-    console.log('name=', ci);
+    // console.log('Spread', array2, per2)
 
-    //ejercicios 05/08/21
-    const json1 = { 1: 'a', 2: 'a', 3: 'a', 4: 'da', 5: 'a', 6: 'a' };
-    const arr1 = Object.keys(json1).map(n => parseInt(n)).filter(n => n % 2 === 0).reduce((acc, value) => acc + value);
-    console.log(arr1);
+    // const per3 = {
+    //   name: 'a',
+    //   age: 12,
+    //   phone: 12323234234,
+    //   extra: 123,
+    //   response: 200,
+    //   response2: 200,
+    //   response4: 200
+    // };
 
-    const arr2 = [1, 2, 3, 4, 5, 6];
-    const res2 = arr2.filter(n => n % 2 === 1).join('');
-    console.log(res2);
+    // const { phone: ci } = per3;
+    // console.log('name=', ci);
+
+    // //ejercicios 05/08/21
+    // const json1 = { 1: 'a', 2: 'a', 3: 'a', 4: 'da', 5: 'a', 6: 'a' };
+    // const arr1 = Object.keys(json1).map(n => parseInt(n)).filter(n => n % 2 === 0).reduce((acc, value) => acc + value);
+    // console.log(arr1);
+
+    // const arr2 = [1, 2, 3, 4, 5, 6];
+    // const res2 = arr2.filter(n => n % 2 === 1).join('');
+    // console.log(res2);
   }
 
-  printData(event: any) {
-    console.log("Ignacio comp: ", event);
+  // printData(event: any) {
+  //   console.log("Ignacio comp: ", event);
+  // }
+
+  // printNames(event: any) {
+  //   console.log("TEST 2: ", event);
+  // }
+
+  addVideo() {
+    this.video++;
+    this.ticktok1.next(this.video);
   }
 
-  printNames(event: any) {
-    console.log("TEST 2: ", event);
+  unsubA() {
+    this.personASub.unsubscribe();
   }
+
 }
