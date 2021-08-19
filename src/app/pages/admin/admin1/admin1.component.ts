24b102165ef644a4458bd763a6a27b904c29418a
IgnacioDelRio <nachitotigredelriozenteno@gmail.com>
2021-08-19 12:57:44 -0400

    18/08/21 3rd task pipes admin

--- src/app/pages/admin/admin1/admin1.component.ts
+++ src/app/pages/admin/admin1/admin1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-admin1',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Admin1Component implements OnInit {
 
+  name: string = 'ignacio';
+  auxJSON = { name: 'ignacio', lastName: 'del rio' };
+  auxObs$ = new BehaviorSubject<number[]>([1, 2, 3]);
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onAdd() {
+    const aux = this.auxObs$.getValue();
+    const aux1 = aux.concat([4, 5]);
+    this.auxObs$.next(aux1);
   }
 
 }
