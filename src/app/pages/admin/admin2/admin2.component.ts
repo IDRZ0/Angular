65c26ae71c5cc5a5d82f46430020277741e9d603
IgnacioDelRio <nachitotigredelriozenteno@gmail.com>
2021-08-19 18:17:43 -0400

    19/08/21 2nd task nullish, optional params, reduce code

--- src/app/pages/admin/admin2/admin2.component.ts
+++ src/app/pages/admin/admin2/admin2.component.ts
@@ -2,14 +2,38 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin2',
-  templateUrl: './admin2.component.html',
-  styleUrls: ['./admin2.component.scss']
+  template: `
+      <p class="hola">admin2</p>
+      <button (click)="testParams()">Test</button>
+      <p>{{test1 ?? test2}}</p>
+      <p>{{test1 || test2}}</p>
+  `,
+  styles: [
+    `
+    .hola{
+      background-color: yellow;
+    }
+    `
+  ]
 })
 export class Admin2Component implements OnInit {
+
+  test1 = 0;
+  test2 = "ignacio";
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  /**
+   * comments
+   */
+  public testParams(name?: string, lastName: string = 'Delrio'): string {
+    const aux = 'nacho';
+    console.log(name + ' ' + lastName);
+    console.log((name ?? aux) + ' ' + lastName);
+    return name + ' ' + lastName;
   }
 
 }
