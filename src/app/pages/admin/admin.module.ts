6b73a1e596c745cbbf6720a413b70e148585ec10
IgnacioDelRio <nachitotigredelriozenteno@gmail.com>
2021-08-22 21:30:55 -0400

    test commit

--- src/app/pages/admin/admin.module.ts
+++ src/app/pages/admin/admin.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Admin1Component } from './admin1/admin1.component';
 import { Admin2Component } from './admin2/admin2.component';
-import { SharedModule } from 'src/app/shared/shared.module';
+import { SharedModule } from '../../shared/shared.module';
 
 
 
--- src/app/pages/buy/buy.module.ts
+++ src/app/pages/buy/buy.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Buy2Component } from './buy2/buy2.component';
 import { Buy1Component } from './buy1/buy1.component';
-import { SharedModule } from 'src/app/shared/shared.module';
+import { SharedModule } from '../../shared/shared.module';
 
 
 
